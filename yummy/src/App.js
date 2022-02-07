b6ba5997644672632b320aecbf204d2f892f3167
Jason Offenbaker <JasonOffenbaker@gmail.com>
2022-02-06 19:37:48 -0700

    Added categories with basic filtering

--- yummy/src/App.js
+++ yummy/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Components/Navbar';
+import Products from './Components/Products'
 
 function App() {
   return (
     <main>
       <Nav></Nav>
+      <Products></Products>
     </main>
   );
 }
